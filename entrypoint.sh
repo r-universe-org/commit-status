c4154caa163c93c53cabc133372b4f9dece54af4
Jeroen Ooms <jeroenooms@gmail.com>
2026-01-02 14:38:04 +0100

    Rename to avoid confusion

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -l
 set -e
 echo "Trying to set commit status for ${REPOSITORY}/commit/${REF}"
-Rscript -e "commitstatus::gh_app_set_commit_status('${REPOSITORY}', '${PACKAGE}', '${REF}','${BUILDLOG}','${UNIVERSE}','${JOBS}')"
+Rscript -e "commitstatus::gh_app_set_commit_status('${REPOSITORY}', '${PACKAGE}', '${REF}','${BUILDLOG}','${UNIVERSE}','${JOBDATA}')"
